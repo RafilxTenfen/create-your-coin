274a01a0784f1b93e8c073bc4965bcbcb49640a1
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-10-27 19:11:57 -0300

    fix inital total supply

--- deploy/01_deployToken.ts
+++ deploy/01_deployToken.ts
@@ -1,12 +1,13 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'; // This adds the type from hardhat runtime environment.
 import { DeployFunction } from 'hardhat-deploy/types'; // This adds the type that a deploy function is expected to fulfill.
+import { BigNumber } from 'ethers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) { // the deploy function receives the hardhat runtime env as an argument
   const {deployments, getNamedAccounts} = hre; // we get the deployments and getNamedAccounts which are provided by hardhat-deploy.
   const { deploy } = deployments; // The deployments field itself contains the deploy function.
 
   const { deployer } = await getNamedAccounts(); // Fetch the accounts. These can be configured in hardhat.config.ts as explained above.
-  const initialSupply = 5000000000000; // value in wei
+  const initialSupply = BigNumber.from('15000000000000000000000'); // value in wei
 
   await deploy('MyErc20Token', { // This will create a deployment called 'Token'. By default it will look for an artifact with the same name. The 'contract' option allows you to use a different artifact.
     from: deployer, // Deployer will be performing the deployment transaction.
